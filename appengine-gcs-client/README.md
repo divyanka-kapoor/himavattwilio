1238d1b4bc57159521bf830a414d5bad260778c0
Divyanka Kapoor <divyanka_kapoor@yahoo.co.in>
2020-04-11 19:37:33 -0500

    M oving to gcloud

---
+++ appengine-gcs-client/README.md
@@ -0,0 +1,23 @@
+![status: inactive](https://img.shields.io/badge/status-inactive-red.svg)
+
+This project is no longer actively developed or maintained.
+
+For new work on this check out:
+* [Google Cloud Java/Cloud Storage](https://github.com/googleapis/google-cloud-java/tree/master/google-cloud-clients/google-cloud-storage)
+* [PyPi/Google Cloud Storage](https://pypi.org/project/google-cloud-storage/)
+
+[![unstable](http://badges.github.io/stability-badges/dist/unstable.svg)](http://github.com/badges/stability-badges)
+
+# AppEngine Google Cloud Storage clients
+
+The libraries in this repository are obsolete and not recommended.
+
+Java Applications that use these APIs can only run in App Engine.
+Applications should upgrade
+to the [Cloud Client Libraries for the Google Cloud Storage API](https://cloud.google.com/storage/docs/reference/libraries)
+before migrating to newer App Engine runtimes.
+The recommended APIs enable your application to run in the Java 8 App Engine
+runtime as well as other environments that support Java 8.
+
+* [Documentation for the Python library](https://cloud.google.com/appengine/docs/python/googlecloudstorageclient/)
+* [Documentation for the Java library](https://cloud.google.com/appengine/docs/java/googlecloudstorageclient/)
--- second_run.py
+++ second_run.py
@@ -3,13 +3,19 @@ import requests
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
+from google.cloud import storage
+import logging
+import wget
 
-cwd = os.getcwd()
-DOWNLOAD_DIRECTORY = cwd
+# cwd = os.getcwd()
+# DOWNLOAD_DIRECTORY = cwd
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS']="./service_account.json"
+
 app = Flask(__name__)
 
-account_sid = 'AC0f3a55e06e1960f649a31d00778c1b1b'
-auth_token = 'ca9ac1a946dc611155998a6db4bcd14c'
+account_sid = 'ACa4a4839e8c88a2d4250621d942f5f1f7'
+auth_token = '2d18ca3fedb5260ae4869cb2ec8ecd56'
 client = Client(account_sid, auth_token)
 
 @app.route('/sms', methods=['GET', 'POST'])
@@ -23,10 +29,41 @@ def sms_reply():
 
         # Use the message SID as a filename.
         filename = request.values['MessageSid']+'.jpg'
-        with open('{}/{}'.format(DOWNLOAD_DIRECTORY, filename), 'wb') as f:
-           image_url = request.values['MediaUrl0']
-           f.write(requests.get(image_url).content)
-           f.close()
+        bucket_name = "staging.semiotic-creek-273921.appspot.com"
+        source_file_name = request.values['MediaUrl0']
+
+        def upload_blob(bucket_name, source_file_name, filename):
+            """Uploads a file to the bucket."""
+
+            filename = wget.download(source_file_name)
+            storage_client = storage.Client()
+            bucket = storage_client.bucket(bucket_name)
+            blob = bucket.blob(filename)
+            blob.upload_from_filename(filename, content_type='image/jpg')
+            os.remove(filename)
+
+        upload_blob(bucket_name, source_file_name, filename)
+
+        print(
+            "File {} uploaded to {}.".format(
+                source_file_name, filename
+            )
+        )
+        # def create_file(self, filename):
+        #     self.response.write('Creating file %s\n' % filename)
+        # write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+        # gcs_file = gcs.open(filename,
+        #               'wb',
+        #               retry_params=write_retry_params)
+        # image_url = request.values['MediaUrl0']
+        # gcs_file.write(requests.get(image_url).content)
+        # gcs_file.write('f'*1024*4 + '\n')
+        # gcs_file.close()
+        # self.tmp_filenames_to_clean_up.append(filename)
+        # with open('{}/{}'.format(DOWNLOAD_DIRECTORY, filename), 'wb') as f:
+        #
+        #    f.write(requests.get(image_url).content)
+        #    f.close()
 
         resp.message("Thanks for your contribution to WILDNET!")
     else:
